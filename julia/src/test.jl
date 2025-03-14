ea718f952eb6d384ce84e5769da4d9fc9e07e742
Sarah Groves <sarahmaddoxgroves@gmail.com>
2025-03-14 13:08:53 -0400

    fixing no output_dir argument for PlutoPages.generate

---
+++ julia/src/test.jl
@@ -0,0 +1,11 @@
+### A Pluto.jl notebook ###
+# v0.17.1
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 617eabf7-fe5f-4faf-b4d5-1b53c5bc289e
+md"i'm new here!"
+
+# ╔═╡ Cell order:
+# ╠═617eabf7-fe5f-4faf-b4d5-1b53c5bc289e
